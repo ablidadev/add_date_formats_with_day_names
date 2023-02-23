b6b0838be60114c31d0b21974b4c5cd4e0130eaf
benl96 <b.lienert@ablida.de>
2023-02-23 13:33:15 +0100

    initial commit

--- init.rb
+++ init.rb
@@ -1,15 +1,17 @@
-require 'settings_helper_patch'
+# frozen_string_literal: true
+
+require File.expand_path('../lib/add_date_formats_with_day_names/settings_helper_patch.rb', __FILE__)
 
 Redmine::Plugin.register :add_date_formats_with_day_names do
   name 'Add Date Formats With Day Names plugin'
   author 'bokos'
   description 'Adds selectable date formats with day names to settings'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/bokos/add_date_formats_with_day_names'
   author_url 'https://github.com/bokos/add_date_formats_with_day_names'
 
   RedmineApp::Application.config.after_initialize do
-    SettingsHelper.send(:include, AddDateFormatsWithDayNames::SettingsHelperPatch)
+    SettingsHelper.include AddDateFormatsWithDayNames::SettingsHelperPatch
   end
 end
 
